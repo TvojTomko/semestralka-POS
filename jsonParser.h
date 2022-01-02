2d135da3aff9df833a73d12c7e1845e210515a0c
TvojTomko <tominotq@gmail.com>
2022-01-02 16:13:33 -0500

    json parser - 0.011

---
+++ jsonParser.h
@@ -0,0 +1,13 @@
+//
+// Created by kali on 1/2/22.
+//
+
+#ifndef SEMESTRALKA_JSONPARSER_H
+
+
+void jsonRead();
+void jsonWrite();
+
+#define SEMESTRALKA_JSONPARSER_H
+
+#endif //SEMESTRALKA_JSONPARSER_H
