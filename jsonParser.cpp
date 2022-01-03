5790d590a7122e72aee2b6a15faceb76e9c3116b
TvojTomko <tominotq@gmail.com>
2022-01-03 15:04:11 -0500

    verzia - 0.12

---
+++ jsonParser.cpp
@@ -0,0 +1,100 @@
+#include <json-c/json.h>
+#include <iostream>
+#include <fstream>
+
+#include "jsonParser.h"
+
+using namespace std;
+
+FILE *fp;
+char buffer[1024];
+
+size_t filesNum;
+
+void jsonRead() {
+    struct json_object *files;
+    struct json_object *parsed_json;
+    struct json_object *fileContent;
+
+    fp = fopen("json-test.txt", "r");
+    fread(buffer, 1024, 1, fp);
+    fclose(fp);
+
+    parsed_json = json_tokener_parse(buffer);
+
+    files = json_object_new_array();
+    json_object_array_add(files, parsed_json);
+
+    filesNum = json_object_array_length(files);
+
+    for (int i = 0; i < filesNum; i++) {
+        fileContent = json_object_array_get_idx(files, i);
+        //json_object_ob
+        cout << "Object " << (i)+1 << ": " << json_object_get_string(fileContent) << endl;
+    }
+
+}
+
+void jsonWrite() {
+    //potom to dorobit na parametre
+    struct json_object *file;
+    struct json_object *parsedjson;
+    struct json_object *array;
+
+    array = json_object_new_array();
+
+    fp = fopen("json-test.txt", "r");
+    fread(buffer, 1024, 1, fp);
+    fclose(fp);
+
+    parsedjson = json_tokener_parse(buffer);
+
+    file = json_object_new_object();
+
+    json_object_object_add(file, "hostname", json_object_new_string("www.asd.sk"));
+    //json_object_object_add(file, "filename", json_object_new_string("images/background.png"));
+    //json_object_object_add(file, "localfilename", json_object_new_string("images/background.png"));
+    //json_object_object_add(file, "path", json_object_new_string("cesta/asd"));
+    //json_object_object_add(file, "size", json_object_new_int(55555));
+    //json_object_object_add(file, "downloaded", json_object_new_int(12345));
+    json_object_object_add(file, "protocol", json_object_new_string("http"));
+    json_object_object_add(file, "priority", json_object_new_int(1));
+
+    json_object_array_add(array, parsedjson);
+    json_object_array_add(array, file);
+
+    json_object_object_add(array,"file", file);
+
+    //json_object_object_add(parsedjson, "file", file);
+
+    json_object_to_file("currentdownload.json", file);
+
+    json_object_to_file("json-test.txt", array);
+}
+
+void jsonDelete() {
+    struct json_object *parsedjson;
+
+    fp = fopen("json-test.txt", "r");
+    fread(buffer, 1024, 1, fp);
+    fclose(fp);
+
+    parsedjson = json_tokener_parse(buffer);
+}
+
+struct json_object * currentJson() {
+    struct json_object *current;
+    struct json_object *parsedjson;
+
+    fp = fopen("json-test.txt", "r");
+    fread(buffer, 1024, 1, fp);
+    fclose(fp);
+
+    parsedjson = json_tokener_parse(buffer);
+
+    current = json_object_new_object();
+
+    json_object_object_add(current, "history", parsedjson);
+
+    return current;
+}
---
+++ jsonParser.h
@@ -0,0 +1,12 @@
+//
+// Created by kali on 1/3/22.
+//
+
+#ifndef SEMESTRALKA_JSONPARSER_H
+#define SEMESTRALKA_JSONPARSER_H
+
+void jsonRead();
+void jsonWrite();
+void jsonDelete();
+
+#endif //SEMESTRALKA_JSONPARSER_H
